dcda804d95f7335cbb128dd0ad057262c703cdc2
S Arya <aryaarun237@gmail.com>
2025-03-06 20:34:50 +0530

    done changes

--- Proj_management(extra)/backend/server.js
+++ Proj_management(extra)/backend/server.js
@@ -1,4 +1,25 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+dotenv.config();
+
+const port = process.env.PORT || 8000;
+const app = express();
+
+//middlewares
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 const server =async () => {
-    console.log('Server is running');
+    try {
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
+        });
+    } catch (error) {
+        console.log("Failed to start server",error.message);
+    }
 };
 server();
